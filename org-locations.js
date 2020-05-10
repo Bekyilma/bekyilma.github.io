5387c9e8298bf38e1baccf9d07246aadffa4541e
Bereket A. YILMA <bekyilma@Berekets-MBP.fritz.box>
2020-05-10 21:20:09 +0200

    Üpdate publications and Talk

---
+++ org-locations.js
@@ -0,0 +1,54 @@
+var addressPoints = [
+  [
+    "Turin, Italy",
+    45.0703,
+    7.6869
+  ],
+  [
+    "Luxembourg, Luxembourg",
+    49.6116,
+    6.1319
+  ],
+  [
+    "Trento, Italy",
+    46.0748,
+    11.1217
+  ],
+  [
+    "Valletta, Malta",
+    35.8989,
+    14.5146
+  ],
+  [
+    "Montreal, Canada",
+    45.508888,
+    -73.561668
+  ],
+  [
+    "Nantes, France",
+    47.2184,
+    1.5536
+  ],
+  [
+    "Vancouver, Canada",
+    49.246292,
+    -123.116226
+  ],
+  [
+    "Nancy, France",
+    48.6921,
+    6.1844
+  ],
+  [
+    "Berlin, Germany",
+    52.5200,
+    13.4050
+  ],
+  [
+    "San Fransisco, USA",
+    37.773972,
+    -122.431297
+  ]
+  
+  
+];
---
+++ talkmap/org-locations.js
@@ -0,0 +1,53 @@
+var addressPoints = [
+  [
+    "Turin, Italy",
+    45.0703,
+    7.6869
+  ],
+  [
+    "Luxembourg, Luxembourg",
+    49.6116,
+    6.1319
+  ],
+  [
+    "Trento, Italy",
+    46.0748,
+    11.1217
+  ],
+  [
+    "Valletta, Malta",
+    35.8989,
+    14.5146
+  ],
+  [
+    "Montreal, Canada",
+    45.5017,
+    73.5673
+  ],
+  [
+    "Nantes, France",
+    47.2184,
+    1.5536
+  ],
+  [
+    "Vancouver, Canada",
+    49.2887,
+    123.1112
+  ],
+  [
+    "Vancouver, Canada",
+    49.2887,
+    123.1112
+  ],
+  [
+    "Nancy, France",
+    48.6921,
+    6.1844
+  ],
+  [
+    "Berlin, Germany",
+    52.5200,
+    13.4050
+  ]
+  
+];
